167246051ffe9ff5a1c1959faae5cabeb045227c
jityoudou <kotobuki8hon@gmail>
2021-04-29 00:54:57 +0900

    マウススピードによってペンの色と大きさが変わる処理

--- src/app/main-canvas/main-canvas.component.ts
+++ src/app/main-canvas/main-canvas.component.ts
@@ -40,19 +40,25 @@ export class MainCanvasComponent implements OnInit {
       p.fill(this.pen_color);
       p.circle(p.mouseX, p.mouseY, this.pen_r);
       
-      this.pen_speed = Math.pow(Math.abs(p.mouseX - p.pmouseX), 2) + Math.pow(Math.abs(p.mouseY - p.pmouseY), 2);
-      //console.log(Math.abs(p.mouseX - p.pmouseX) < 2 && Math.abs(p.mouseY - p.pmouseY) < 2);
-      console.log(this.pen_speed);
-      if (Math.abs(p.mouseX - p.pmouseX) <= 3 && Math.abs(p.mouseY - p.pmouseY) <= 3) {
-        if (this.pen_r > 5) {
-          this.pen_r -= 1;
-        }
-        this.pen_color = this.blue;
-      } else {
-        this.pen_r += 1;
-        this.pen_color = this.red;
+      this.pen_speed = Math.sqrt(Math.pow(Math.abs(p.mouseX - p.pmouseX), 2) + Math.pow(Math.abs(p.mouseY - p.pmouseY), 2));
+      if (this.pen_speed > 50) {
+        this.pen_speed = 50;
       }
       
+      //console.log(Math.abs(p.mouseX - p.pmouseX) < 2 && Math.abs(p.mouseY - p.pmouseY) < 2);
+      
+      console.log(this.pen_r);
+      //console.log(this.pen_color);
+      
+      this.pen_color = [this.pen_speed / 50 * 255, 0, (1 - this.pen_speed / 50) * 255];
+
+      if (this.pen_speed <= 5) {
+        if (this.pen_r > 5) {
+          this.pen_r -= 2;
+        } else { }
+      } else {
+        this.pen_r += 2;
+      }     
 
 
       if (this.reset_flag==true) {
